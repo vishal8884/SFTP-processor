88b19b877c923aa0677ae7b6be926fad85179807
vishal <vishalambekar459@gmail.com>
2022-10-09 00:11:49 +0530

    read sftp code added

--- SftpToJavaProject/src/main/java/io/vishal/service/ReadSftp.java
+++ SftpToJavaProject/src/main/java/io/vishal/service/ReadSftp.java
@@ -1,14 +1,20 @@
 package io.vishal.service;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.integration.sftp.session.SftpSession;
 import org.springframework.stereotype.Service;
 
 import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.ChannelSftp.LsEntry;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -30,6 +36,7 @@ public class ReadSftp {
 		try {
 			session = sftpConnectionService.connectToSftpServer();
 			channel = session.getClientInstance();
+			readFileFromServer(channel, session);
 			
 		} catch (Exception e) {
 			log.error("exception occured while connectinf to sftp server");
@@ -48,6 +55,33 @@ public class ReadSftp {
 		String fileName = null;
 		
 		try {
+			ChannelSftp.LsEntry[] lsEntries = sftpConnectionService.getFileEntryFromSftpRate(sftpSession);
+			
+			Comparator<LsEntry> comparator = Comparator.comparing(ls -> ls.getAttrs().getMTime());
+			List<LsEntry> sortedLsEntries = Arrays.asList(lsEntries).stream().sorted(comparator).collect(Collectors.toList());
+			
+			filesList = sortedLsEntries.stream().map(ls -> ls.getFilename()).collect(Collectors.toList());
+			
+			log.debug("filesList :: "+filesList);
+			
+			if(filesList.isEmpty()) {
+				log.info("no files found to read");
+			}
+			else {
+				for(ChannelSftp.LsEntry lsEntry : sortedLsEntries) {
+					fileName = lsEntry.getFilename();
+					if(!fileName.endsWith(".txt")) {
+						log.error("invalid file type please use .txt files");
+						continue;
+					}
+					String fileLocation = String.format("%s%s","/sftp/",fileName);
+					log.debug("filename :: "+fileName+"       fileLocation :: "+fileLocation);
+					
+					inputStream = sftpConnectionService.getFileInputStreamFromSftp(channelSftp, fileLocation);
+					bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+					readFileLineByLine(bufferedReader, fileName);
+				}
+			}
 			
 		}
 		catch (Exception e) {
@@ -66,6 +100,13 @@ public class ReadSftp {
 		}
 	}
 	
+	private void readFileLineByLine(BufferedReader bufferedReader, String fileName) throws IOException {
+		String line = null;
+		
+		while((line = bufferedReader.readLine()) != null) {
+			log.debug("line :: "+line);
+		}
+	}
 	
 	private void closeSftpConnection(SftpSession session, ChannelSftp channel) {
 		log.info("closing sftp session");
--- SftpToJavaProject/src/main/java/io/vishal/service/SftpConnectionService.java
+++ SftpToJavaProject/src/main/java/io/vishal/service/SftpConnectionService.java
@@ -1,8 +1,15 @@
 package io.vishal.service;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.springframework.integration.sftp.session.DefaultSftpSessionFactory;
 import org.springframework.integration.sftp.session.SftpSession;
 import org.springframework.stereotype.Service;
+
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.ChannelSftp.LsEntry;
+import com.jcraft.jsch.SftpException;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,20 +17,36 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class SftpConnectionService {
 	
+	private String host = "127.0.0.1";
+	private int port = 22;
+	private String user = "user";
+	private String password = "password";
+	private String folderPath = "/sftp/";
+	
+	
 	public SftpSession connectToSftpServer() {
 		log.info("connectToSftpServer() called");
 		return getDefaultSftpSessionFactory().getSession();
 	}
 	
+	public LsEntry[] getFileEntryFromSftpRate(SftpSession session) throws IOException {
+		log.info("accessing the list of files in sftp folder");
+		
+		return session.list(folderPath);
+	}
+	
+	public InputStream getFileInputStreamFromSftp(ChannelSftp channel,String fileLocation) throws SftpException {
+		return channel.get(fileLocation);
+	}
 	
 	
 	private DefaultSftpSessionFactory getDefaultSftpSessionFactory() {
 		DefaultSftpSessionFactory factory = new DefaultSftpSessionFactory();
 		
-		factory.setHost("127.0.0.1");
-		factory.setPort(22);
-		factory.setUser("user");
-		factory.setPassword("password");
+		factory.setHost(host);
+		factory.setPort(port);
+		factory.setUser(user);
+		factory.setPassword(password);
 		factory.setAllowUnknownKeys(true);
 		
 		return factory;
